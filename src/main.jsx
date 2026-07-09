e755764516e51e544c0239fd34bbfc45a8c5b9b3
suevaniocdf-glitch <suevaniocdf@gmail.com>
2026-07-08 23:30:10 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Se não tiver este arquivo, pode remover esta linha
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
